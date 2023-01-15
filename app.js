6b9f3725b6ea397de6d639144eeaabc5bd01c1d4
basagoitiam <marika.basagoitia@gmail.com>
2023-01-15 14:42:08 -0500

    finished setting up server

--- app.js
+++ app.js
@@ -1,8 +1,45 @@
-console.log("Hello World!\n==========\n");
-
-// Exercise 1 Section
-console.log("EXERCISE 1:\n==========\n");
-
 const http = require("http");
 
-// Finish setting up the server
+http
+  .createServer(function (req, res) {
+    let contentType = "text/html";
+    let responseBody = "default";
+
+    req.on("error", function (err) {
+      res.writeHead(400, { "Content-Type": "text/html" });
+      res.write("An error occurred because of your request : (");
+      res.end();
+    });
+    res.on("error", function (err) {
+      res.writeHead(500, { "Content-Type": "text/html" });
+      res.write("An error occurred on the server : (");
+      res.end();
+    });
+    
+    const chunks = [];
+
+    req.on("data", (chunk) => {
+      chunks.push(chunk);
+    })
+
+    req.on("end", () => {
+      body = Buffer.concat(chunks).toString();
+      if (req.url == "/") {
+        responseBody = "Homepage";
+      } else if (req.url == "/about") {
+        contentType = "application/json";
+        responseBody = JSON.stringify({name: "Marika", age: 24, location: "New Haven, CT"});
+      } else if (req.url = "/echo") {
+        contentType = "application/json";
+        responseBody = JSON.stringify({method: req.method, url: req.url, body: body});
+      } else {
+        responseBody = "<h1>Page Not Found.<h1><br><a href = '/'>Go to home</a>";
+      }
+      res.writeHead(200, { "Content-Type": contentType});
+      res.write(responseBody);
+      res.end();
+    });
+  })
+  .listen(3000, () => {
+    console.log("listening on port 3000");
+  });
